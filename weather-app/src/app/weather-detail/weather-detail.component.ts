275fb910aeb19d016cc3bb716740d699e76a467c
Dominic Bronnenkant <dominic.bronnenkant@meinautohaus.de>
2025-03-10 16:14:11 +0100

    setup detail component

--- weather-app/src/app/weather-detail/weather-detail.component.ts
+++ weather-app/src/app/weather-detail/weather-detail.component.ts
@@ -1,23 +1,45 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, signal, WritableSignal } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+
+import { WeatherService } from '../weather.service';
 
 @Component({
   selector: 'app-weather-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './weather-detail.component.html',
   styleUrl: './weather-detail.component.css',
 })
 export class WeatherDetailComponent {
   private route = inject(ActivatedRoute);
+  private weatherService = inject(WeatherService);
+
   city: string = '';
   weatherData: WritableSignal<{ temp: number; description: string } | null> =
     signal(null);
 
   constructor() {
     this.city = this.route.snapshot.paramMap.get('city') || 'Unbekannt';
-    // Wetterdaten setzen
-    this.weatherData.set({ temp: 20, description: 'Sonnig' });
+    this.fetchWeatherData();
+  }
+
+  private fetchWeatherData() {
+    this.weatherService.getCityCoordinates(this.city).subscribe((response) => {
+      if (response?.results?.length) {
+        const { latitude, longitude } = response.results[0];
+
+        this.weatherService
+          .getWeather(latitude, longitude)
+          .subscribe((data) => {
+            this.weatherData.set({
+              temp: data.current_weather.temperature,
+              description: `Wind: ${data.current_weather.windspeed} km/h`,
+            });
+          });
+      } else {
+        alert('Stadt nicht gefunden!');
+      }
+    });
   }
 }
